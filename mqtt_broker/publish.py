672167620975911fe525d74401712507e48c8273
Sam Joel <samjoel3101@gmail.com>
2023-02-26 17:11:39 +0530

    Add docker-compose for mqtt broker

--- mqtt_broker/publish.py
+++ mqtt_broker/publish.py
@@ -1,16 +1,14 @@
 import paho.mqtt.publish as publish
 
-from .constants import MQTT_PATH, MQTT_SERVER
+from .constants import MQTT_PATH, MQTT_SERVER, MQTT_PORT
 
 
 def publish_image(byteArr):
-    publish.single(MQTT_PATH, byteArr, hostname=MQTT_SERVER)
+    publish.single(MQTT_PATH, byteArr, hostname=MQTT_SERVER, port=MQTT_PORT)
 
 
 if __name__ == "__main__":
-    f = open(
-        "/home/samjoel/Projects/mlops_on_edge/mqtt_broker/data/94644be7-8080-408f-aca5-21e25724a64d.png", "rb"
-    )
+    f = open("/home/samjoel/Projects/mlops_on_edge/mqtt_broker/data/94644be7-8080-408f-aca5-21e25724a64d.png", "rb")
     fileContent = f.read()
     byteArr = bytearray(fileContent)
     publish_image(byteArr)
